9194937c8c25331430cb8768e143e5266b13e8d0
LaraSanchoBlanco <larasancho13@gmail.com>
2022-08-18 10:50:27 +0200

    ejercicio 2 SQL

--- SQL/ejercicio2_consultas_multitablas_1.sql
+++ SQL/ejercicio2_consultas_multitablas_1.sql
@@ -8,21 +8,32 @@ GROUP BY customer_id;
 
 # EJERCICIO 2:
 USE northwind;
-SELECT YEAR(orders.order_date),customers.contact_name AS 'nombre empresa', COUNT(order_details.quantity)
-FROM orders JOIN order_details JOIN customers
-WHERE country= 'UK'
-GROUP BY customers.contact_name;
 
-customers- customer_id
-orders - customer_id
-contact_name (nombre de empresa)
-country(para fiktrar por el pais)
+SELECT customers.company_name AS 'EMPRESA', YEAR(orders.order_date) AS 'AÑO', SUM(order_details.quantity) AS 'CANTIDAD', customers.country
+FROM customers INNER JOIN orders 
+ON customers.customer_id = orders.customer_id 
+INNER JOIN order_details
+ON orders.order_id = order_details.order_id 
+WHERE country= 'UK' 
+GROUP BY order_details.order_id;
 
+#EJERCICIO 3:
+SELECT customers.company_name AS 'EMPRESA', YEAR(orders.order_date) AS 'AÑO', SUM(order_details.quantity) AS 'CANTIDAD',
+CASE
+	WHEN order_details.discount = 0 THEN (order_details.unit_price * order_details.quantity) 
+    WHEN order_details.discount <> 0 THEN (order_details.unit_price * order_details.quantity) * order_details.discount
+    END AS total_dinero
 
+FROM order_details INNER JOIN orders 
+ON orders.order_id = order_details.order_id 
+INNER JOIN customers
+ON customers.customer_id = orders.customer_id
+GROUP BY order_details.order_id;
 
+# BONUS 4:
 
-orders- order_id
-orders_detail - order_id
-quantity
-order_date(agrupado por año)
+SELECT customers.company_name AS 'EMPRESA', orders.order_id, orders.order_date AS 'FECHA'
+FROM customers INNER JOIN orders
+USING (customer_id);
+
 
